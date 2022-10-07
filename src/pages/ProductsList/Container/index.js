3fa0a84cee12f7a2d9d05b653c263b89fe4d9967
GspaceDevelopers <115160413+GspaceDevelopers@users.noreply.github.com>
2022-10-07 14:10:45 -0300

    Atualizando lista de produtos metodo delete

--- src/pages/ProductsList/Container/index.js
+++ src/pages/ProductsList/Container/index.js
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Styles.css";
 import Modal from "./Modal/modal";
+import api from "../../../services/api";
+import { toast } from "react-toastify";
 
 const Container = (props) => {
+  const [produtosFilter, setProdutosFilter] = useState([]);
+  useEffect(() => {
+    fetch("https://apigspace.herokuapp.com/produtos")
+      .then((response) => response.json())
+      .then((data) => {
+        setProdutosFilter(data);
+      });
+  }, []);
+  let config = {
+    headers: {
+      Authorization: "Access-Control-Allow-Origin",
+    },
+  };
+  async function delItem(id) {
+    await api
+      .delete(`/produtos/${id}`, config)
+      .then(() => {
+        toast.success("Excluido com sucesso");
+        window.location.reload();
+      })
+      .catch((error) => {
+        toast.error(error);
+      });
+  }
   return (
     <div className="produto-center">
       <div className="produto-list">
@@ -11,8 +37,15 @@ const Container = (props) => {
           <div className="produto-info">
             <h2>{props.tittle}</h2> <h2>R${props.preco}</h2>
           </div>
-          <div className="modal-ajust">
-            <Modal testId={props.id} produtos={props.tittle} />
+          <div className="ajust-button-list">
+            <div className="modal-ajust">
+              <Modal testId={props.id} produtos={props.tittle} />
+            </div>
+            <div>
+              <button id="delete-product" onClick={() => delItem(props.id)}>
+                Delete
+              </button>
+            </div>
           </div>
         </div>
       </div>
--- src/pages/ProductsList/index.js
+++ src/pages/ProductsList/index.js
@@ -6,13 +6,29 @@ import SideBar from "../../Components/SideBar";
 
 const ListProducts = () => {
   const [produtos, setProdutos] = useState([]);
+  const [filterValue, SetFilterValue] = useState([]);
+  const [produtosFilter, setProdutosFilter] = useState([]);
   useEffect(() => {
     fetch("https://apigspace.herokuapp.com/produtos")
       .then((response) => response.json())
       .then((data) => {
         setProdutos(data);
+      })
+      .catch((error) => {
+        console.log(produtos.categoria);
+        console.log(error);
       });
   }, []);
+
+  // FilterApi
+  function FilterProducts(filterValue) {
+    let ArrayProducts = produtos.filter((item) => {
+      return item.categoria == filterValue;
+    });
+
+    setProdutos(ArrayProducts);
+  }
+
   return (
     <>
       {" "}
@@ -20,13 +36,20 @@ const ListProducts = () => {
       <div className="side-product">
         <SideBar />
         <div className="list-products">
+          <input
+            type="text"
+            onChange={(e) => SetFilterValue(e.target.value)}
+            id=""
+          />
+          <button onClick={FilterProducts}>teste</button>
           {produtos.map((produto) => {
             return (
               <Container
                 key={produto._id}
                 image={produto.imgurl}
-                tittle={produto.produto}
+                tittle={produto.modelo}
                 preco={produto.preco}
+                category={produto.categoria}
                 id={produto._id}
               />
             );
